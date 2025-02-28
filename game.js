11ddd5acc5e347bc911fb03be2be64434b4c2d9f
Gummysaur <superjulialbrecht8@yahoo.com>
2025-02-28 17:22:59 -0500

    added neil, removed old placeholder assets

--- game.js
+++ game.js
@@ -3,9 +3,10 @@ import Seal from "./Seal.js";
 
 let kyoro;
 let yochan;
+let neil;
 
 let icehole;
-let score = 0;
+let score = 2000;
 let scoreAllTime = 0;
 let fpc = 1; // fish per click
 let fps = 0; // idle fish gained per second
@@ -24,6 +25,7 @@ let toolTipText;
 let fpsUpgrade;
 let fpcUpgrade;
 let yochanUpgrade;
+let neilUpgrade;
 
 let sc;
 
@@ -34,7 +36,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 0 },
             debug: false
         }
     },
@@ -51,11 +53,7 @@ function preload ()
 {
     sc = this;
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('icehole', 'assets/icehole.png');
-    this.load.image('seal', 'assets/seal.png');
     this.load.image('fish', 'assets/fish.png');
-    this.load.image('yochan', 'assets/yochan.png');
-    this.load.image('babyseal', 'assets/babyseal.png');
     this.load.image('bait', 'assets/bait.PNG');
     this.load.image('fisherman', 'assets/fisherman.PNG');
     this.load.image('newseal', 'assets/newseal.PNG');
@@ -63,11 +61,14 @@ function preload ()
     this.load.spritesheet('babysprite', 'assets/babysprite.png', {frameWidth: 98, frameHeight: 98});
     this.load.spritesheet('iceholesprite', 'assets/iceholesprite.png', {frameWidth: 98, frameHeight: 98});
     this.load.spritesheet('yochansprite', 'assets/yochansprite.png', {frameWidth: 98, frameHeight: 98});
+    this.load.spritesheet('babyneilsprite', 'assets/babyneilsprite.png', {frameWidth: 98, frameHeight: 98});
+    this.load.spritesheet('neilsprite', 'assets/neilsprite.png', {frameWidth: 98, frameHeight: 98});
 }
 
 function create ()
 {
     this.add.image(400, 300, 'sky');
+    this.add.rectangle(50, 390, 700, 160, 0xffffff).setOrigin(0);
     scoreBoard = this.add.text(20, 30, 'Fish: ' + score, {
         fontFamily: 'serif', 
         fontSize: '24px'
@@ -111,18 +112,42 @@ function create ()
         yoyo: true
     })
 
+    this.anims.create({
+        key: 'banana',
+        frames: this.anims.generateFrameNumbers('babyneilsprite', {start: 0, end: 9}),
+        frameRate: 10,
+        repeat: -1,
+        repeatDelay: 10000,
+        yoyo: true
+    })
+
+    this.anims.create({
+        key: 'rest',
+        frames: this.anims.generateFrameNumbers('neilsprite', {start: 0, end: 11}),
+        frameRate: 10,
+        repeat: -1,
+        repeatDelay: 12000,
+        yoyo: true
+    })
+
     icehole = this.add.sprite(400, 175, 'iceholesprite').setInteractive({ useHandCursor: true });
     icehole.anims.play('water');
 
     kyoro = new Seal("Kyoro", "Spotted Seal", 'Female', -1, ' ',
-        this.add.sprite(400, 300, 'kyorosprite').setInteractive(), 'adult'
-    )
+        this.add.sprite(400, 300, 'kyorosprite').setInteractive({ useHandCursor: true }), 'adult'
+    );
     kyoro.sprite.anims.play('blink');
 
-    yochan = yochan = new Seal("Yochan", "Ringed Seal", "Female", 500, ' ',
-        this.add.sprite(1000, 300, 'babysprite').setInteractive(), 'baby'
-    )
+    yochan = new Seal("Yochan", "Ringed Seal", "Female", 500, ' ',
+        this.add.sprite(1000, 300, 'babysprite').setInteractive({ useHandCursor: true }), 'baby'
+    );
     yochan.sprite.anims.play('yawn');
+
+    neil = new Seal('Neil', "Elephant Seal", "Male", 1000, ' ', 
+        this.add.sprite(550, 300, 'babyneilsprite').setInteractive({useHandCursor: true}), 'baby'
+    );
+    neil.sprite.visible = false;
+    neil.sprite.anims.play('banana');
 
     fpsUpgrade = new Upgrade(10, 
         this.add.image(100, 475, 'fisherman').setInteractive({ useHandCursor: true }),
@@ -137,7 +162,13 @@ function create ()
     yochanUpgrade = new Upgrade(100,
         this.add.image(400, 475, 'newseal').setInteractive({ useHandCursor: true }),
         '  Seal: A new seal for you! \n Costs 100 fish.'
-    )
+    );
+
+    neilUpgrade = new Upgrade(700,
+        this.add.image(400, 475, 'newseal').setInteractive({useHandCursor: true}),
+        '  Seal: Another new seal for you! \n Costs 700 fish.'
+    );
+    neilUpgrade.sprite.visible = false;
 
     toolTip =  this.add.rectangle(900, 700, 300, 100, 0xffffff).setOrigin(0);
     toolTipText = this.add.text(900, 700, 'placeholder', { fontFamily: 'Arial', color: '#000' }).setOrigin(0);
@@ -163,6 +194,7 @@ function create ()
         yochanUpgrade.hover = false;
         kyoro.hover = false;
         yochan.hover = false;
+        neil.hover = false;
         toolTip.setPosition(900, 700);
         toolTipText.setPosition(900, 700);
     });
@@ -179,13 +211,21 @@ function create ()
         yochanUpgrade.onHover(toolTip, toolTipText, pointer);
     });
 
+    neilUpgrade.sprite.on('pointermove', function (pointer){
+        neilUpgrade.onHover(toolTip, toolTipText, pointer);
+    });
+
     kyoro.sprite.on('pointermove', function (pointer, x, y, event){
         kyoro.onHover(toolTip, toolTipText, pointer);
-    })
+    });
 
     yochan.sprite.on('pointermove', function(pointer, x, y, event){
         yochan.onHover(toolTip, toolTipText, pointer);
-    })
+    });
+
+    neil.sprite.on('pointermove', function(pointer){
+        neil.onHover(toolTip, toolTipText, pointer);
+    });
 
     fpsUpgrade.sprite.on('pointerdown', () => {
         if(score >= fpsUpgrade.cost){
@@ -217,17 +257,96 @@ function create ()
             yochanUpgrade.sprite.destroy();
             toolTip.setPosition(900, 700);
             toolTipText.setPosition(900, 700);
+            neilUpgrade.sprite.visible = true;
         }
     });
 
+    neilUpgrade.sprite.on('pointerdown', () => {
+        if(score >= neilUpgrade.cost){
+            score -= neilUpgrade.cost;
+            neilUpgrade.level++;
+            neil.sprite.visible = true;
+            neilUpgrade.sprite.destroy();
+            toolTip.setPosition(900, 700);
+            toolTipText.setPosition(900, 700);
+        }
+    })
+
+    kyoro.sprite.on('pointerdown', () => {
+        this.tweens.add({
+            targets: kyoro.sprite,
+            y: '-=15',
+            ease: 'Cubic',
+            duration: 240,
+            repeat: 0,
+            yoyo: false,
+            onComplete: function(){
+                sc.tweens.add({
+                    targets: kyoro.sprite,
+                    y: 300,
+                    ease: 'Bounce',
+                    duration: 600,
+                    repeat: 0,
+                    yoyo: false
+                })
+            }
+        });
+    })
+
     yochan.sprite.on('pointerdown', () => {
-        if(score >= yochan.lvlUpCost){
+        if(score >= yochan.lvlUpCost && yochan.stage === 'baby'){
             score -= yochan.lvlUpCost;
             yochan.lvl++;
             yochan.stage = 'adult';
             yochan.sprite.setTexture('yochansprite');
             yochan.sprite.anims.play('look');
         }
+        this.tweens.add({
+            targets: yochan.sprite,
+            y: '-=20',
+            ease: 'Cubic',
+            duration: 100,
+            repeat: 0,
+            yoyo: false,
+            onComplete: function(){
+                sc.tweens.add({
+                    targets: yochan.sprite,
+                    y: 300,
+                    ease: 'Bounce',
+                    duration: 600,
+                    repeat: 0,
+                    yoyo: false
+                })
+            }
+        });
+    })
+
+    neil.sprite.on('pointerdown', () => {
+        if(score >= neil.lvlUpCost && neil.stage === 'baby'){
+            score -= neil.lvlUpCost;
+            neil.lvl++;
+            neil.stage = 'adult';
+            neil.sprite.setTexture('neilsprite');
+            neil.sprite.anims.play('rest');
+        }
+        this.tweens.add({
+            targets: neil.sprite,
+            y: '-=10',
+            ease: 'Cubic',
+            duration: 340,
+            repeat: 0,
+            yoyo: false,
+            onComplete: function(){
+                sc.tweens.add({
+                    targets: neil.sprite,
+                    y: 300,
+                    ease: 'Bounce',
+                    duration: 600,
+                    repeat: 0,
+                    yoyo: false
+                })
+            }
+        });
     })
 
     for (let d = 0; d < clickBuffer; d++) {
@@ -313,10 +432,22 @@ function update (time, delta)
             '\n  A spoiled diva.'
         );
         if(yochan.stage === 'baby'){
-            toolTipText.appendText('Needs ' + yochan.lvlUpCost + ' fish to grow up.'
-            )
+            toolTipText.appendText('Needs ' + yochan.lvlUpCost + ' fish to grow up.');
         }
         else if(yochan.stage === 'adult'){
+            toolTipText.appendText('All grown up!');
+        }
+    }
+    if(neil.hover){
+        toolTipText.setText('Name: ' + neil.name +
+            '\nSpecies: ' + neil.species +
+            '\nGender: ' + neil.gender +
+            '\n Stubborn and short-tempered.'
+        );
+        if(neil.stage === 'baby'){
+            toolTipText.appendText('Needs ' + neil.lvlUpCost + ' fish to grow up.');
+        }
+        else if(neil.stage === 'adult'){
             toolTipText.appendText('All grown up!');
         }
     }
